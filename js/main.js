4a2cc23c7aeab8fdc3e7174c800d0520c2f0511f
Diego <diegomugnae@gmail.com>
2020-02-19 21:11:57 +1000

    Trying to filter array of countries :(

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ function chama(){
     for (i in countriesList) {
         console.log(countriesList[i].countryName);
       }      
-}
+};
+
+function lookForCountry(){
+  var output = [];
+  for (let i=0; i < countriesList.length ; i++){
+    output.push(countriesList[i].countryName);
+  }
+  return output;
+};
+
